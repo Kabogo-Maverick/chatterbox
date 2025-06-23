a23fb560d7b5a1d8b2a52b2478a2aa963d667e0a
mkay <mkay@gmail.com>
2025-06-23 19:19:15 +0300

    Complete Chatterbox lab with working Flask API

--- server/seed.py
+++ server/seed.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from random import choice as rc
-
 from faker import Faker
 
 from app import app
@@ -14,20 +13,18 @@ if "Duane" not in usernames:
     usernames.append("Duane")
 
 def make_messages():
-
     Message.query.delete()
-    
-    messages = []
 
-    for i in range(20):
-        message = Message(
+    messages = [
+        Message(
             body=fake.sentence(),
             username=rc(usernames),
         )
-        messages.append(message)
+        for _ in range(20)
+    ]
 
     db.session.add_all(messages)
-    db.session.commit()        
+    db.session.commit()
 
 if __name__ == '__main__':
     with app.app_context():
